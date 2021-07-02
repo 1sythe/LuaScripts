a002f133f85d759b3a766304ee126e0aa1612422
Luca <83220423+kiisuhh@users.noreply.github.com>
2021-07-02 21:07:55 +0200

    Update VIP Script.lua
    removed comments

--- Roblox/ServerScriptStorage/VIP Script.lua
+++ Roblox/ServerScriptStorage/VIP Script.lua
@@ -1,12 +1,12 @@
-local gamepassId = 16512469 -- Gamepass ID
+local gamepassId = 16512469 
 local service = game:GetService("MarketplaceService")
 
 game.Players.PlayerAdded:Connect(function(player)
 	if (service:UserOwnsGamePassAsync(player.UserId, gamepassId)) then
 		local tags = {
 			{
-				TagText = "VIP", -- Tag
-				TagColor = Color3.fromRGB(255, 255, 0) -- VIP Color
+				TagText = "VIP", 
+				TagColor = Color3.fromRGB(255, 255, 0) 
 			}
 		}
 		local ChatService = require(game:GetService("ServerScriptService"):WaitForChild("ChatServiceRunner").ChatService)
@@ -17,6 +17,6 @@ game.Players.PlayerAdded:Connect(function(player)
 			wait(0.01)
 		end
 		speaker:SetExtraData("Tags",tags)
-		speaker:SetExtraData("ChatColor",Color3.fromRGB(255, 255, 0)) -- Text Color
+		speaker:SetExtraData("ChatColor",Color3.fromRGB(255, 255, 0)) 
 	end
 end)
